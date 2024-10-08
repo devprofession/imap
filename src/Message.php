b3ef248979dc39872f08da46870f444f22894189
arpad.horvath <arpad.horvath@profession.hu>
2024-10-08 14:15:11 +0200

    bugfix

--- src/Message.php
+++ src/Message.php
@@ -319,15 +319,6 @@ class Message extends Message\Part
             }
         );
 
-        // Ellenőrizzük, hogy az üzenet létezik-e
-        $messageNo = imap_msgno($this->stream, $this->messageNumber);
-        if ($messageNo === false) {
-            // Ha az üzenet nem létezik, dobjunk exceptiont
-            restore_error_handler();
-            throw new MessageDoesNotExistException($this->messageNumber, "Message does not exist");
-        }
-
-        // Ha az üzenet létezik, folytatjuk a struktúra betöltését
         $structure = imap_fetchstructure(
             $this->stream,
             $this->messageNumber,
